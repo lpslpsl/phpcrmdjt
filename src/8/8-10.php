b26a18132d28a34ed6f34df23a9c9ccf9dd5028b
lpslpsl <1245441817@qq.com>
2017-06-05 14:18:42 +0800

    练习

---
+++ src/8/8-10.php
@@ -0,0 +1,12 @@
+<form method="post">
+    <input type="file" name="file" size="15">
+    <input type="submit">
+</form>
+<?php
+/** file
+ * Created by PhpStorm.
+ * User: $lps
+ * Date: 2017/6/5
+ * Time: 13:46
+ */
+echo $_POST["file"];
---
+++ src/8/8-11.php
@@ -0,0 +1,4 @@
+<a href="8-11.php?id=<?php echo urlencode("php编程词典");?>">跳转</a>
+<?php
+
+echo urldecode($_GET["id"]);
